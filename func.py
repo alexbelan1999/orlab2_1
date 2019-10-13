65d9d45215b630a2ca2f7257ed7325b853f6c21a
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-13 18:10:04 +0300

    Add TMatrix method

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+import math
 def TMatrix(A):
     m = len(A)
     n = len(A[0])
@@ -6,3 +7,19 @@ def TMatrix(A):
         for j in range(0,m):
             AT[i][j] = A[j][i]
     return AT
+
+def Adjacency_matrix(nodes,edges_with_weight):
+    vertex = len(nodes)
+    adj_matrix = [[None] * (vertex + 1) for i in range(vertex + 1)]
+
+    for i in range(1, vertex + 1):
+        for j in range(1, vertex + 1):
+            if (i == j):
+                adj_matrix[i][j] = 0
+            else :
+                adj_matrix[i][j] = math.inf
+
+    for k in range(0,len(edges_with_weight)):
+        adj_matrix[edges_with_weight[k][0]][edges_with_weight[k][1]] = edges_with_weight[k][2]
+
+    return adj_matrix
