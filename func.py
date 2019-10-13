670fcd3cdbfa8147025b4dfdd86b7e8a6c317634
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-13 20:27:14 +0300

    Add Floyd method

--- func.py
+++ func.py
@@ -60,8 +60,8 @@ def Dijkstra(nodes, edges_with_weight, start, end):
         return result
 
     ind = 0
-
     i = end
+
     while (i != start):
         res = []
         res.append(int_arr[i])
@@ -71,6 +71,57 @@ def Dijkstra(nodes, edges_with_weight, start, end):
         i = int_arr[i]
         ind += 1
 
+    result.reverse()
+
+    return result
+
+def Floyd(nodes, edges_with_weight, start, end):
+    vertex = len(nodes)
+    adj_matrix = Adjacency_matrix(nodes, edges_with_weight)
+    d = Adjacency_matrix(nodes, edges_with_weight)
+
+    int_arr = [[0] * (vertex + 1) for i in range(vertex + 1)]
+    for i in range(1, vertex + 1):
+        for j in range(1, vertex + 1):
+            int_arr[i][j] = i
+
+    for k in range(1, vertex + 1):
+        for i in range(1, vertex + 1):
+            for j in range(1, vertex + 1):
+                len1 = d[i][k] + d[k][j]
+                if (len1 > 0 and len1 < d[i][j]):
+                    d[i][j] = len1;
+                    int_arr[i][j] = k;
+        print('Шаг ',k,':')
+        print('L(' , k ,'):')
+        print()
+        for dp in d:
+            print(dp)
+        print('S(' , k , '):')
+        for arr in int_arr:
+            print(arr)
+
+    result =[]
+
+    if (d[start][end] == math.inf):
+        return result
+
+    v = end
+    ind = 0
+
+    while (int_arr[start][v] != start):
+        res = []
+        res.append(int_arr[start][v])
+        res.append(v)
+        res.append(adj_matrix[int_arr[start][v]][v])
+        result.append(res)
+        ind += 1
+        v = int_arr[start][v]
+    res = []
+    res.append(start)
+    res.append(v)
+    res.append(adj_matrix[int_arr[start][v]][v])
+    result.append(res)
     result.reverse()
 
     return result
