905b01b762024aea1592cc6bb06020b040303a48
alexbelan1999 <alexbelan1999@gmail.com>
2020-04-18 16:20:13 +0300

    Fix Floyd method

--- func.py
+++ func.py
@@ -1,14 +1,17 @@
 import math
+
+
 def TMatrix(A):
     m = len(A)
     n = len(A[0])
     AT = [[0] * m for i in range(n)]
-    for i in range(0,n):
-        for j in range(0,m):
+    for i in range(0, n):
+        for j in range(0, m):
             AT[i][j] = A[j][i]
     return AT
 
-def Adjacency_matrix(nodes,edges_with_weight):
+
+def Adjacency_matrix(nodes, edges_with_weight):
     vertex = len(nodes)
     adj_matrix = [[None] * (vertex + 1) for i in range(vertex + 1)]
 
@@ -16,13 +19,14 @@ def Adjacency_matrix(nodes,edges_with_weight):
         for j in range(1, vertex + 1):
             if (i == j):
                 adj_matrix[i][j] = 0
-            else :
+            else:
                 adj_matrix[i][j] = math.inf
 
-    for k in range(0,len(edges_with_weight)):
+    for k in range(0, len(edges_with_weight)):
         adj_matrix[edges_with_weight[k][0]][edges_with_weight[k][1]] = edges_with_weight[k][2]
 
     return adj_matrix
+
 
 def Dijkstra(nodes, edges_with_weight, start, end):
     vertex = len(nodes)
@@ -46,14 +50,14 @@ def Dijkstra(nodes, edges_with_weight, start, end):
         if (v == end):
             break
 
-        print('Рассматривается вершина: ',v)
+        print('Рассматривается вершина: ', v)
 
         for j in range(1, vertex + 1):
             edge_weight = adj_matrix[v][j]
             if (edge_weight < math.inf and (d[v] + edge_weight < d[j])):
                 d[j] = d[v] + edge_weight
                 int_arr[j] = v
-            print('l`(', j ,') = ', d[j])
+            print('l`(', j, ') = ', d[j])
 
     result = []
     if (d[end] == math.inf):
@@ -75,6 +79,7 @@ def Dijkstra(nodes, edges_with_weight, start, end):
 
     return result
 
+
 def Floyd(nodes, edges_with_weight, start, end):
     vertex = len(nodes)
     adj_matrix = Adjacency_matrix(nodes, edges_with_weight)
@@ -93,23 +98,23 @@ def Floyd(nodes, edges_with_weight, start, end):
                     d[i][j] = len1;
                     int_arr[i][j] = k;
         print()
-        print('Шаг ',k,':')
-        print('L(' , k ,'):')
+        print('Шаг ', k, ':')
+        print('L(', k, '):')
 
         test = 0
         for dp in d:
-            if test !=0:
+            if test != 0:
                 print(dp[1:])
             test += 1
 
         test = 0
-        print('S(' , k , '):')
+        print('S(', k, '):')
         for arr in int_arr:
-            if test !=0:
+            if test != 0:
                 print(arr[1:])
             test += 1
 
-    result =[]
+    result = []
 
     if (d[start][end] == math.inf):
         return result
--- lab2_1.py
+++ lab2_1.py
@@ -1,14 +1,14 @@
-import math
-import numpy as np
-import networkx as nx
 import matplotlib.pyplot as plt
+import networkx as nx
+
 import func
 
-nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+nodes = [1, 2, 3, 4]
 var = 0
 start = 0
 end = 0
-while var not in [1,2]:
+while var not in [1, 2]:
     var = int(input("Введите 1 для метода Дейкстры или 2 для метода Флойда: "))
 
 while start not in nodes:
@@ -16,25 +16,25 @@ while start not in nodes:
 
 while end not in nodes:
     end = int(input("Введите конечную вершину: "))
-
-edges_with_weight = [(1,2,10), (1,4,5), (2,3,3), (2,5,9) , (3,6,11), (4,2,5), (4,5,10), (4,7,6), (4,8,15), (5, 6,13), (5,8,10), (5,9,5), (6,9,7), (7,8,8), (8,9,4)]
+edges_with_weight = [(1, 2, 1), (1, 3, 6), (2, 3, 4), (2, 4, 1), (4, 3, 1)]
+# edges_with_weight = [(1,2,10), (1,4,5), (2,3,3), (2,5,9) , (3,6,11), (4,2,5), (4,5,10), (4,7,6), (4,8,15), (5, 6,13), (5,8,10), (5,9,5), (6,9,7), (7,8,8), (8,9,4)]
 G = nx.DiGraph()
 G.add_nodes_from(nodes)
 G.add_weighted_edges_from(edges_with_weight)
 
 layout = nx.spring_layout(G)
-nx.draw(G,layout,with_labels=True,node_color='b')
+nx.draw(G, layout, with_labels=True, node_color='b')
 labels = nx.get_edge_attributes(G, "weight")
-nx.draw_networkx_edge_labels(G,pos=layout, edge_labels=labels)
+nx.draw_networkx_edge_labels(G, pos=layout, edge_labels=labels)
 plt.show()
 
 if var == 1:
     algoritm_d = func.Dijkstra(nodes, edges_with_weight, start, end)
     if len(algoritm_d) == 0:
-        print("Нет пути из вершины ",start , " в вершину ", end)
-    else :
+        print("Нет пути из вершины ", start, " в вершину ", end)
+    else:
         sum_d = 0
-        for i in range(0,len(algoritm_d)):
+        for i in range(0, len(algoritm_d)):
             sum_d += algoritm_d[i][2]
 
         algoritm_d = func.TMatrix(algoritm_d)
@@ -42,7 +42,7 @@ if var == 1:
         str_path_d = ' --> '.join(str(x) for x in algoritm_d[0])
 
         print("Алгоритм Дейкстры: ")
-        print("Путь: " , str_path_d , "-->" , end , ", длина пути = " , sum_d)
+        print("Путь: ", str_path_d, "-->", end, ", длина пути = ", sum_d)
 
         nodes1.append(end)
         dedge = []
@@ -63,7 +63,7 @@ if var == 1:
 elif var == 2:
     algoritm_f = func.Floyd(nodes, edges_with_weight, start, end)
     if len(algoritm_f) == 0:
-        print("Нет пути из вершины ", start , " в вершину " , end)
+        print("Нет пути из вершины ", start, " в вершину ", end)
     else:
         sum_d = 0
         for i in range(0, len(algoritm_f)):
@@ -74,7 +74,7 @@ elif var == 2:
         str_path_f = ' --> '.join(str(x) for x in algoritm_f[0])
 
         print("Алгоритм Флойда: ")
-        print("Путь: " , str_path_f , "-->" , end , ", длина пути = " , sum_d)
+        print("Путь: ", str_path_f, "-->", end, ", длина пути = ", sum_d)
         nodes1.append(end)
         dedge = []
         for d in edges_with_weight:
